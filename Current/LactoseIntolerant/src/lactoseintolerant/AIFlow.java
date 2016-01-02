b095484838f54ff1ac47636c2d29089c5ebc0d30
Josh <Josh@EpicWizardStaff>
2016-01-01 22:36:19 -0600

    took out a few unused variables

--- Current/LactoseIntolerant/src/lactoseintolerant/AIFlow.java
+++ Current/LactoseIntolerant/src/lactoseintolerant/AIFlow.java
@@ -15,7 +15,6 @@ import java.awt.image.BufferedImage;
  * @author 0001058857
  */
 public class AIFlow {
-    public boolean wipedOut=false,exploded=false;
     
     //keep the same between types::::              VVVVVVVVVV
     public int TYPE;
@@ -25,20 +24,15 @@ public class AIFlow {
         
     public boolean accelerating=false,brakes=false;
     
-    public boolean attacking=false,canAttack=true,isDoneWithAttack=false;
-    public int canAttackPing=0,attackReachPing=50;
-    
     public double speed=30;
     //has no angle max/min values (will go straight unless hit by another vehicle; will not turn on its own.
     public double angle=0;//angle 0=straight up
-    
     
     public double noEffectDecrease=3,brakeDecrease=5;
     public int currentTurnRate=0;
     
     public int angleIncrement=5;
     
-    public float distanceTravelled=0;
     public int[] screenLocation={180,400};
     public int[] IMG_BLANK_SPACE=new int[2];
     
@@ -58,7 +52,7 @@ public class AIFlow {
     public int ADD_FOR_HTNG_R_SIDE=45,ADD_FOR_HTNG_L_SIDE=25;
     public int ADD_FOR_LEFT_MEDIAN=45,ADD_FOR_RIGHT_MEDIAN=25;
     public int[] imageSize;
-    public int[][] originalPoints;//for polygon
+//    public int[][] originalPoints;//for polygon
                                                        //^^^^^
     
     public BufferedImage currentImage=null;
